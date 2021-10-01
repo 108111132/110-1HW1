58e8591bc61a1bbd6fb0beade98e4a64ec9a93d5
沂萱 柳 <108111132@mail.aeust.edu.tw>
2021-10-01 19:26:08 +0800

    hw1

---
+++ 110-1HW1/Trans.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1HW1
+{
+    public partial class Trans : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            double c, d_value;
+            d_value = 3.928;
+            c = d_value * d_value;
+            response.write("d_value =" + d_value + "*" + d_value + "=" + c);
+          
+        }
+    }
+}
